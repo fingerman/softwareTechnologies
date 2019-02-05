c89f3fcfb35822aa086e6f2e7c6769ec352eaf76
fingerman <adamov.george@gmail.com>
2019-02-05 19:56:30 +0100

    blog-aspnet

---
+++ csharp-asp/blog-aspnet/Blog/Readme.md
@@ -0,0 +1,6 @@
+
+
+
+Check for SQL Server LocalDB Installation  
+cmd -> sqllocaldb  
+If missing ? download and install SQL Server LocalDB  
---
+++ csharp-asp/blog-aspnet/upload/Blog/Controllers/ArticleController.cs
@@ -0,0 +1,113 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: Article/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                //get articles from db
+                var articles = database.Articles.Include(a => a.Author).ToList();
+
+                return View(articles);
+            }
+                
+        }
+
+        // GET: Article/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                var article = database.Articles.Where(a => a.Id == id).Include(a => a.Author).First();
+
+                return View(article);
+
+            }
+        }
+
+        // GET: Article/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Article/Create
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+
+                using (var database = new BlogDbContext())
+                {
+                    var authorId = database.Users.Where(u => u.UserName == this.User.Identity.Name).First().Id;
+
+                    article.AuthorId = authorId;
+
+                    database.Articles.Add(article);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(article);
+        }
+
+        // GET: Article/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+
+
+                var article = database.Articles.Where(a => a.Id == id).Include(a => a.Author).First();
+
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                
+
+                return View(article);
+
+            }
+        }
+
+
+
+    }
+}
