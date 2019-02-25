1ac4886be5e4920874d0ef09cd73edbd803475c1
fingerman <adamov.george@gmail.com>
2019-02-25 11:50:36 +0100

    accounts

--- python_django/articles/views.py
+++ python_django/articles/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Article
+from django.http import HttpResponse
 
 
 # Create your views here.
 def articles_list(request):
-    return render(request, 'articles/articles_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/articles_list.html', {'articles': articles})
+
+
+def article_details(request, slug):
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article': article})
+
+
+def article_create(request):
+    return render(request, 'articles/article_create.html')
+
--- python_django/python_django/settings.py
+++ python_django/python_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_mysql',
     'articles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+
+# static files are in the base -> assets Folder
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
