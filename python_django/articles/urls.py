0f3ecde2b2714f129205d25387ae65173cfbacee
fingerman <adamov.george@gmail.com>
2019-02-27 16:35:51 +0100

    blog ready without styling

--- python_django/articles/urls.py
+++ python_django/articles/urls.py
@@ -6,6 +6,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.articles_list, name="list"),
-    path('create/', views.article_create, name="create"),
+    path('create', views.article_create, name="create"),
     path('<slug:slug>', views.article_details, name="detail"),
 ]
